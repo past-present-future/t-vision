e7176fda854215c7458cf2be1db7e7529e4615a7
idea-exp <vlatko.saravanja@proton.me>
2025-01-06 18:45:41 +0000

    Automatic pickup for the virtual camera setup, saving progress

--- resources/setup_resources.sh
+++ resources/setup_resources.sh
@@ -1,4 +1,38 @@
 #!/bin/bash 
-sudo modprobe v4l2loopback devices=1 video_nr=0 card_label='big_buck_bunny_steam_YUV-320x180' exclusive_caps=1
-v4l2-ctl --list-devices
+#OUTPUT="$(v4l2-ctl --list-devices)"
+MODULE="v4l2loopback"
+CAMERA_NAME="natureYUV_steam_YUV-320x180"
+
+if lsmod | grep -wq "$MODULE"; then
+    echo "$MODULE is loaded!"
+else
+    echo "$MODULE is not loaded!\n Loading module...\n"
+    OUTPUT="$(ls /dev/ | grep video)"
+    if [[ $OUTPUT != "" ]];then
+       echo $OUTPUT
+       IFS="o" read loc ind <<< "$OUTPUT"
+       echo "Index got: $ind"
+       echo "Test got: $loc"
+       VIDEO_NR=$(eval $ind + 1)
+    else
+	echo "No video device setting binding to /dev/video0"
+	VIDEO_NR=0
+    fi
+    echo "No video device setting binding to /dev/video0"
+    sudo modprobe v4l2loopback devices=1 video_nr=0 card_label="$CAMERA_NAME" exclusive_caps=1
+fi
+
+
+#OUTPUT="$(v4l2-ctl --list-devices | grep video?)"
+
+#echo $OUTPUT
+#if [[ "$OUTPUT" == *"$CAMERA_NAME"* ]]; then
+    #echo "Camera already here"
+#else
+    #echo $(echo $OUTPUT | grep video\*)
+    #sudo modprobe v4l2loopback devices=1 video_nr=0 card_label="$CAMERA_NAME" exclusive_caps=1
+#fi
+
 ffmpeg -stream_loop -1 -re -i nature_320x180.mp4 -f v4l2 -vcodec rawvideo -pix_fmt yuv420p /dev/video0 
+sudo modprobe -r v4l2loopback
+
