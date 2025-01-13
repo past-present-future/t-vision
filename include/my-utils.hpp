98b5135eebf67ba6dff3068c2f468af2ec61ddf9
idea-exp <vlatko.saravanja@proton.me>
2025-01-13 15:41:22 +0000

    Implemented ring list, delayed overlay playback program, used second YUV pipeline with ring list for ordering data

--- include/my-utils.hpp
+++ include/my-utils.hpp
@@ -1,7 +1,33 @@
 #ifndef MY_UTILS_HPP 
-#define MY_UTILS_HPP 
+#define MY_UTILS_HPP
 #include <iostream>
 
-std::string load_shader_from_file(const std::string& filename);
 
+std::string load_shader_from_file(const std::string& filename);
+namespace mu {
+template <typename T> class Node {
+public:  
+  Node *left, *right;
+  T data;
+  Node(T data);
+};
+template <typename  T>
+class RingList{
+private:
+  Node<T> *items;
+  size_t elem_num;
+
+public:
+  RingList();
+  size_t get_number_of_elements();
+  void add_left(T data);
+  void add_elem(T data);
+  T pop_elem();
+  T pop_right();
+  
+  };
+
+}; // namespace mu
+
+#include "../lib/my-utils.cpp"
 #endif /* MY-UTILS_HPP */
--- lib/my-utils.cpp
+++ lib/my-utils.cpp
@@ -13,3 +13,45 @@ std::string load_shader_from_file(const std::string& filename)
 	buffer << file.rdbuf();
 	return buffer.str();
 }
+
+template<typename T>
+mu::RingList<T>::RingList(){
+  this->items =  nullptr;
+  elem_num = 0;
+}
+
+template <typename T> mu::Node<T>::Node(T value) { this->data = value; }
+
+template <typename T >
+void mu::RingList<T>::add_elem(T data)
+{
+  Node<T> *tmp = new Node(data);
+  if (items != nullptr) {
+
+    tmp->left = items->left;
+    tmp->right = tmp; 
+    items->left = tmp;
+  }
+  else {
+    tmp->left = tmp;
+    tmp->right = tmp;
+  }  
+  items = tmp;
+  ++this->elem_num;
+ }
+
+template <typename T>
+T mu::RingList<T>::pop_elem(){
+  Node<T> *left = this->items->left;
+  this->items->left = left->left;
+  T value = left->data;
+  delete[] left;
+  --this->elem_num;  
+  return value;
+}
+
+
+template <typename T >
+size_t mu::RingList<T>::get_number_of_elements(){
+  return this->elem_num;
+  }
